0ff0d0f84fe6c5d287ad3ef4997f1c57780784f0
kovalovaanastasiia <tokach26@gmail.com>
2023-08-02 12:16:27 +0300

    bug fixed

--- middlewars/authenticate.js
+++ middlewars/authenticate.js
@@ -13,7 +13,7 @@ const authenticate = async (req, res, next) => {
     const {authorization = ''} = req.headers;
     const [bearer, token] = authorization.split(' ');
 
-    if (bearer !== "Bearer") {
+    if (token && bearer !== "Bearer") {
         throw HttpError(401)
     }
     try {
